494d7d3ab4903c47449481781effd27524cd61d2
Bhat <shreyasb@UMICH.EDU>
2022-06-28 09:44:55 -0400

    Minor plot additions

--- analyze_data.py
+++ analyze_data.py
@@ -66,18 +66,18 @@ def analyze(data: dict):
     mean = np.mean(trust_feedback, axis=0)
     std = np.std(trust_feedback, axis=0)
 
-    fig, ax = plot(fig, ax, mean.flatten(), std.flatten(), 'tab:blue', plot_label='feedback', title="Trust after each site", x_label='Mission index', y_label='Trust')
+    fig, ax = plot(fig, ax, mean.flatten(), std.flatten(), 'tab:blue', plot_label='feedback', title="Trust after each site", x_label='Site index', y_label='Trust')
     
     trust_estimate = data['trust estimate']
     mean = np.mean(trust_estimate, axis=0)
     std = np.std(trust_estimate, axis=0)
 
-    fig, ax = plot(fig, ax, mean.flatten(), std.flatten(), 'tab:orange', plot_label='estimate', title="Trust after each site", x_label='Mission index', y_label='Trust')
+    fig, ax = plot(fig, ax, mean.flatten(), std.flatten(), 'tab:orange', plot_label='estimate', title="Trust after each site", x_label='Site index', y_label='Trust')
 
     ax.legend()
     ax.set_ylim([-0.05, 1.05])
 
-    ########################################### POSTERIOR ###############################
+    ########################################### POSTERIOR ################################################
     fig, ax = plt.subplots()
 
     posterior_dists = data['posterior']
@@ -88,6 +88,28 @@ def analyze(data: dict):
 
     # mean and std have shape (num_missions, N+1)
     fig, ax = plot_posterior(fig, ax, weights, mean, std)
+
+    ######################################### Mean weight after each site #################################################
+    fig, ax = plt.subplots()
+
+    wh_means = data['mean health weight']   # Shape (num_simulations, num_missions, N+1)
+    mean = np.mean(wh_means, axis=0)        # Shape (num_missions, N+1)
+    std = np.std(wh_means, axis=0)          # Shape (num_missions, N+1)
+
+    fig, ax = plot(fig, ax, mean.flatten(), std.flatten(), 'tab:blue', plot_label='mean weight', title="Estimated health weight after each site", x_label='Site index', y_label='Mean health weight')
+
+    ax.set_ylim([-0.05, 1.05])
+
+    ########################################### Mean weight after each mission ############################################
+
+    fig, ax = plt.subplots()
+
+    wh_means = data['mean health weight']   # Shape (num_simulations, num_missions, N+1)
+    mean = np.mean(wh_means, axis=0)        # Shape (num_missions, N+1)
+    std = np.std(wh_means, axis=0)          # Shape (num_missions, N+1)
+
+    fig, ax = plot(fig, ax, mean[:, -1], std[:, -1], 'tab:blue', plot_label='mean weight', title="Estimated health weight after each mission", x_label='Mission index', y_label='Mean health weight')
+    ax.set_ylim([-0.05, 1.05])
 
     plt.show()
 
